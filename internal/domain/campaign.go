4dca2ff604a77a2ea5c02886f15f64c62f57acce
anggara setiawan <aanggaadc@gmail.com>
2026-05-01 13:29:49 +0700

    add domain campaign

---
+++ internal/domain/campaign.go
@@ -0,0 +1,66 @@
+package domain
+
+import (
+	"errors"
+	"time"
+)
+
+type Campaign struct {
+	ID          string
+	UserID      string
+	Name        string
+	Platform    string
+	Impressions int
+	Clicks      int
+	Conversions int
+	Cost        float64
+	DateStart   time.Time
+	DateEnd     time.Time
+}
+
+func NewCampaign(
+	userID, name, platform string,
+	impressions, clicks, conversions int,
+	cost float64,
+	start, end time.Time,
+) (*Campaign, error) {
+
+	campaign := &Campaign{
+		ID:          "", // nanti diisi repository
+		UserID:      userID,
+		Name:        name,
+		Platform:    platform,
+		Impressions: impressions,
+		Clicks:      clicks,
+		Conversions: conversions,
+		Cost:        cost,
+		DateStart:   start,
+		DateEnd:     end,
+	}
+
+	if err := campaign.Validate(); err != nil {
+		return nil, err
+	}
+
+	return campaign, nil
+}
+
+func (c *Campaign) Validate() error {
+	if c.Name == "" {
+		return errors.New("name is required")
+	}
+
+	if c.Platform == "" {
+		return errors.New("platform is required")
+	}
+
+	if c.Impressions < 0 || c.Clicks < 0 || c.Conversions < 0 {
+		return errors.New("metrics cannot be negative")
+	}
+
+	if c.DateEnd.Before(c.DateStart) {
+		return errors.New("end date cannot be before start date")
+	}
+
+	return nil
+}
