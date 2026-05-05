9c5881125b231ef79d62692f85ba0d12ffda8d9e
anggara setiawan <aanggaadc@gmail.com>
2026-05-05 14:23:05 +0700

    fix: create prepared statement issue

--- internal/infrastructure/database/postgres.go
+++ internal/infrastructure/database/postgres.go
@@ -2,22 +2,24 @@ package database
 
 import (
 	"context"
+	"log"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func NewPostgresPool(databaseURL string) *pgxpool.Pool {
-	config, err := pgxpool.ParseConfig(databaseURL)
+func NewPostgresPool(connString string) *pgxpool.Pool {
+	config, err := pgxpool.ParseConfig(connString)
 	if err != nil {
-		panic(err)
+		log.Fatalf("unable to parse config: %v", err)
 	}
 
-	config.ConnConfig.DefaultQueryExecMode = pgx.QueryExecModeSimpleProtocol
+	config.ConnConfig.StatementCacheCapacity = 0
+	config.ConnConfig.DefaultQueryExecMode = pgx.QueryExecModeDescribeExec
 
 	pool, err := pgxpool.NewWithConfig(context.Background(), config)
 	if err != nil {
-		panic(err)
+		log.Fatalf("unable to connect to database: %v", err)
 	}
 
 	return pool
